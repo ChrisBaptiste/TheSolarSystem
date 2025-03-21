56f168df39ef94f0784af65af2f1a77822b69d1f
Urick_Baptiste <chrisbaptiste19@gmail.com>
2025-03-21 09:23:59 -0400

    my second commit

--- index.js
+++ index.js
@@ -209,13 +209,13 @@ solarSystem.forEach(planet => {
   console.log(totalSatellites);
   
   // 4. Write a conditional that checks if Mercury's radiusp is a string (remember the typeof command). If Mercury's radiusp is a string, change the value to be the same as its radiuse value. (radiusp is radius at the poles, radiuse is radius at the equator).
-  if(typeof system.mercury.radiusp === "sting"){
+  if(typeof system.mercury.radiusp === "string"){
     system.mercury.radiusp = system.mercury.radiuse;
   }
   console.log(system.mercury.radiusp);
   
   // 5. Console.log Mercury's radiusp (should be 2439, not 'same').
-  console.log(system.mercury.radiusp); // this is confusing it still prints out "same" to console.
+  console.log(system.mercury.radiusp); 
   
   // 6. Find the difference between Jupiter's momentum and Pluto's momentum. (To get momentum, you'll want the product of velocity and mass).
   const jupiterMomentum = system.jupiter.velocity * system.jupiter.mass;
